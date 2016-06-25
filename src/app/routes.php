6b5cdd1ada30d57cae1e73682019cf638c3afa6d
Lucas Cherkewski <lucas@ecustom.ca>
2016-06-25 13:11:21 -0400

    write a quick PoC to query books on a shelf

--- src/app/routes.php
+++ src/app/routes.php
@@ -4,3 +4,13 @@ $app->get('/', function($request, $response, $args) {
     return 'Hello!';
 })->setName('index');
 
+$app->get('/shelf/{userId}/{shelfName}', function($request, $response, $args) {
+    return $this->api->get('review/list',
+        ['query' => array_merge($this->api->getConfig('query'), [
+            'v' => 2,
+            'id' => $args['userId'],
+            'shelf' => $args['shelfName'],
+        ])]
+    );
+});
+
