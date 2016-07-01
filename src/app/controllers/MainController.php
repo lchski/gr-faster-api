d821a4f2b226343475d88ba102e2a9edd10b82f1
Lucas Cherkewski <lucas@ecustom.ca>
2016-07-01 16:43:51 -0400

    process shelf api call to just include the fields we want

--- src/app/controllers/MainController.php
+++ src/app/controllers/MainController.php
@@ -4,17 +4,28 @@
 namespace Lchski;
 
 
+use Illuminate\Support\Collection;
 use Lchski\Contracts\Controller;
 
 class MainController extends BaseController implements Controller
 {
     public function getShelves()
     {
-        return $this->buildResponse(
-            $this->callApi('shelf/list.xml', [
-                'user_id' => $this->args['userId'],
-            ])
-        );
+        $data = $this->callApi('shelf/list.xml', [
+            'user_id' => $this->args['userId'],
+        ]);
+
+        $dataCollection = collect($this->xmlResponseToArray($data)['GoodreadsResponse']['shelves']['user_shelf']);
+
+        $processedData = $dataCollection->map(function ($item, $key) {
+            return [
+                'id' => $item['id']['$'],
+                'name' => $item['name'],
+                'book_count' => $item['book_count']['$'],
+            ];
+        });
+
+        return $this->buildResponse($processedData);
     }
 
     public function getShelfBooks()
